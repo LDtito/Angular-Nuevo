0a15ad9f440af2f9abafb29b2458fba6c489b3ee
jean <jean@gmail.com>
2023-01-11 08:07:15 -0500

    probando

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -7,17 +7,33 @@ import {HttpClient} from '@angular/common/http';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent {
-  constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient) {};
 
+  
+  getProducts():void{
+    const url ="https://api.escuelajs.co/api/v1/products";
+    const response = this.httpClient.get(url).subscribe
+    (response => {console.log(response);
+    });
+  }
+
+  getProduct(){
+    const url ="https://api.escuelajs.co/api/v1/products";
+    const response = this.httpClient.get(url).subscribe
+    (response => {console.log(response);
+    });
+  }
+
+  createProduct(){
+    const url = "https://api.escuelajs.co/api/v1/products";
+    this.httpClient.post(url, data).subscribe(
+      response => {console.log(response);}
+    );
   }
 
   ngOnInit(): void{
-    this.getProducts();
+    //this.getProducts();
+    this.getProduct();
   }
-  getProducts(){
-    const response = this.httpClient.get('https://api.escuelajs.co/api/v1/products').subscribe
-    (response => {console.log(response);
-    }
-    );
-  }
+
 }
