e066a426a9c06af1ef0afeb4dc1b1c33a2d394b5
LDtito <henry.jl.live@gmail.com>
2023-01-22 16:08:33 -0500

    creando servicio

--- src/app/services/product-http-service.service.ts
+++ src/app/services/product-http-service.service.ts
@@ -10,32 +10,32 @@ export class ProductHttpServiceService {
   readonly API_URL ="https://api.escuelajs.co/api/v1/products";
 
   constructor(private httpClient: HttpClient) {}
-
+  //traer todos los productos
   getAll():Observable<ProductModel>{
     const url = `${this.API_URL}`;
     return this.httpClient.get<ProductModel>(url);
   }
-
+  //traer un producto
   getOne(id:ProductModel['id']):Observable<ProductModel>{
     const url =`${this.API_URL}/${id}`;
     return this.httpClient.get<ProductModel>(url);
   }
-
+  //Crear un producto
   store(product:CreateProductDto):Observable<ProductModel>{
     const url = `${this.API_URL}`;
     return this.httpClient.post<ProductModel>(url, product)
   }
-
+  //Actualizar un producto
   update(id:ProductModel['id'], product:UpdateProductDto):Observable<ProductModel>{
     const url = `${this.API_URL}/${id}`;
     return this.httpClient.put<ProductModel>(url, product)
   }
-
+  //Eliminar un producto
   destroy(id:ProductModel['id']):Observable<any>{
     const url = `${this.API_URL}/${id}`;
     return this.httpClient.delete<any>(url).pipe(map((response: {rta:boolean}) =>{
       return response.rta;
     })
-    );  
+    );
   }
 }
